83820e64dfa43623f2366d0352424383a6e6b3b6
Vivek <vivekparde2411@gmail.com>
2023-07-24 11:39:08 +0530

    initialize react project with tailwind config

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+// import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import ProjectListingPage from "./components/ProjectListingPage"
+import Navbar from "./components/Navbar"
+import Category from "./components/Category"
+import Cart from "./components/Cart"
+import ProductDetail from "./components/ProductDetail"
+import Context from "./context/Context"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Context>
+          <Navbar />
+          <div>
+            <Routes>
+              <Route exact path="/" element={<ProjectListingPage />} />
+              <Route exact path="/category" element={<Category />} />
+              <Route exact path="/detail/:id" element={<ProductDetail />} />
+              <Route exact path="/cart" element={<Cart />} />
+            </Routes>
+          </div>
+        </Context >
+      </BrowserRouter>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
