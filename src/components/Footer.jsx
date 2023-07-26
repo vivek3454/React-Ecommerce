5f01f3cbb3a2a86f8183c9e3f971512d3de98b6c
Vivek <vivekparde2411@gmail.com>
2023-07-26 13:02:37 +0530

    created footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-10 mb-5 font-semibold text-lg text-center'>Made with ❣️ by Vivek</div>
+  )
+}
+
+export default Footer
