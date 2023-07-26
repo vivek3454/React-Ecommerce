c26f806f90439604a4ce72c4e038e0f1ebb5ba43
Vivek <vivekparde2411@gmail.com>
2023-07-26 13:02:11 +0530

    created category section

---
+++ src/components/Category.jsx
@@ -0,0 +1,88 @@
+import React, { useContext } from 'react'
+import Carousel from 'react-multi-carousel';
+import "react-multi-carousel/lib/styles.css";
+import { AppContext } from '../context/context';
+import ProductCard from './ProductCard';
+import { categories } from '../data/categories';
+
+const Category = () => {
+  const { products, setProducts } = useContext(AppContext);
+
+  return (
+    <>
+      <div className='container mx-auto'>
+        {
+          categories.map((category) => {
+            if (category.value === 'all') {
+              return
+            }
+            return (
+              <CategoryCarousel
+                key={category.label}
+                products={products}
+                label={category.label}
+                value={category.value}
+              />
+            )
+          })
+        }
+
+      </div>
+    </>
+  )
+}
+
+export default Category
+
+const CategoryCarousel = ({ products, label, value }) => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+  return (
+    <>
+      <h2 className='mt-12 text-4xl font-semibold'>{label}</h2>
+      <Carousel
+        responsive={responsive}
+        arrows
+        renderArrowsWhenDisabled={false}
+        className=""
+        focusOnSelect={true}
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="dotList"
+      >
+        {
+          products.filter((item) => item.category === value)
+            .map((product) => (
+              <ProductCard
+                key={product.id}
+                category={product.category}
+                id={product.id}
+                title={product.title}
+                price={product.price}
+                description={product.description}
+                image={product.images[0]}
+                rating={product.rating}
+              />
+            ))
+
+        }
+      </Carousel>
+    </>
+  )
+}
