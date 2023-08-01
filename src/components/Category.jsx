8349da1a814d69b94f8d5aebd7c35047aa2783ad
Vivek <vivekparde2411@gmail.com>
2023-08-01 17:27:06 +0530

    added react slick

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,25 +1,23 @@
-import React, { useContext } from 'react'
-import Carousel from 'react-multi-carousel';
+import React from 'react'
 import "react-multi-carousel/lib/styles.css";
-import { AppContext } from '../context/context';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import ProductCard from './ProductCard';
 import { categories } from '../data/categories';
+import { product } from '../data/products';
 
 const Category = () => {
-  const { products } = useContext(AppContext);
 
   return (
     <>
-      <div className='container mx-auto'>
+      <div className='bg-white min-[375px]:px-10 max-[375px]:px-5'>
         {
           categories.map((category) => {
-            if (category.value === 'all') {
-              return
-            }
             return (
               <CategoryCarousel
                 key={category.label}
-                products={products}
+                products={product}
                 label={category.label}
                 value={category.value}
               />
@@ -35,37 +33,47 @@ const Category = () => {
 export default Category
 
 const CategoryCarousel = ({ products, label, value }) => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1124,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 610,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1,
+        }
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          centerMode: false,
+          slidesToShow: 1,
+        }
+      }
+    ]
   };
+
   return (
     <>
       <h2 className='mt-12 ml-5 text-4xl font-semibold'>{label}</h2>
-      <Carousel
-        responsive={responsive}
-        arrows
-        renderArrowsWhenDisabled={false}
-        className="ml-4"
-        focusOnSelect={false}
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        dotListClass="dotList"
-      >
+
+      <Slider {...settings} >
         {
           products.filter((item) => item.category === value)
             .map((product) => (
@@ -82,7 +90,7 @@ const CategoryCarousel = ({ products, label, value }) => {
             ))
 
         }
-      </Carousel>
+      </Slider>
     </>
   )
 }
